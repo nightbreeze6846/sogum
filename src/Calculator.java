34ee2a50beb5f3960df4f8c23f56062218895750
Park <gabriel6846@gmail.com>
2015-03-18 00:24:17 +0900

    no message

---
+++ src/Calculator.java
@@ -0,0 +1,12 @@
+
+public class Calculator {
+	static int add(int a, int b){
+		return a+b;
+	}
+	static int sub(int a, int b){
+		return a-b;
+	}
+	static int mul(int a, int b){
+		return a*b;
+	}
+}
---
+++ src/main.java
@@ -0,0 +1,8 @@
+
+public class main {
+	public static void main(String[] args){
+		System.out.println(Calculator.add(7,4));
+		System.out.println(Calculator.sub(7,4));
+		System.out.println(Calculator.mul(7,4));
+	}
+}
