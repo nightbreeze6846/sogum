427df1625cf98b38aa2ebe68b5bddcb36586b5e1
Park <gabriel6846@gmail.com>
2015-03-18 01:05:31 +0900

    no message

--- src/main.java
+++
@@ -1,8 +0,0 @@
-
-public class main {
-	public static void main(String[] args){
-		System.out.println(Calculator.add(7,4));
-		System.out.println(Calculator.sub(7,4));
-		System.out.println(Calculator.mul(7,4));
-	}
-}
